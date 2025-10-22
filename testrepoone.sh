f31e68067732bc29c346af3bfbc780ddb0da65b5
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-10-22 11:05:28 +0530

    hello world

---
+++ testrepoone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Srikanth :: Hi Bro How are u?"
+echo "Bro:: Hey Srikanth, Fine.. What about you"
+echo "Srikanth :: Fine Thanks, What are you doing now"
+echo "Bro :: Nothing Srikanth"
