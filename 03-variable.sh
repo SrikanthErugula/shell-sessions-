4e860470e912db781006f572fd4f6ddd163d160b
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-10-24 11:52:07 +0530

    hello world

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # so ikkada person1 ki meeru emi assign cheste adhi adi refelct avuthundhi
+# use enti ante evrytime velli 4 lines lo changes cheyalante not at all easy for everytime, ippudu ante 4 lines vunnnai adhe 400 vunte we can't do 
+# so andhuke manam variables ni introduce chesam... variable means to hold something.
+#no space before and after =
+# $ symbol use enti ante varibale use chestunam in code so something special kosam use chestam
 
 Person1=SRI
 Person2=Reddy
