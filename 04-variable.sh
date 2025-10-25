036f44d3976ebd587c0ed21b0e0e11b6c2fb604e
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-10-25 07:02:53 +0530

    hello world

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # variable names eppudu kuda a meaningful ga vundali ante ardham ayyala vundali, 
 # Arguments bayata numchi pass cheyatum ante manaki nachhina names bayata numci ayina pass cheeyachhu
+# in every script lo this type of option vunttadhi,manam enni ayina pass cheyachhu like $1,$2,$3......$n mana istum 
+# $ history ane cmd iste, it will shows all history upto now.
 PERSON1=$1
 PERSON2=$2
 
