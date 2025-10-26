1c92b498c4f802ad8c6b5ae1b241dcef8c8716ee
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-10-26 20:32:58 +0530

    hello world

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -5,7 +5,11 @@ NUMBER1=100
 NUMBER2=200
 NAME=DevOps
 
-SUM=$(($NUMBER1+$NUMBER2+$NAME))
+Number1=2
+number2=4
+SUM=$(($number1+$number2))
+
+SUM=$(($NUMBER1+$NUMBER2+$NAME)) # so ila run cheste only additon operation jarigi 300 print avuthunhi, name print avvadhu bcz it automatically shell anedhi think chesi wrong ichharu ani zero ga consider chesi print cheyadhu
 
 echo "SUM is: ${SUM}"
 
