f355a6f4f006dfc3b44b504e3d15e12e697bbe75
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-10-26 19:50:56 +0530

    hello world

--- 07-cmdvariable.sh
+++ 07-cmdvariable.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "Timestamp is: $DATE"
+#echo "Timestamp is: $DATE"
+
+
+START_TIME=$(date +%s)
+
+sleep 10 # So here (sleep 10 &) iste backend lo sleep avuthundhi ala kakunda sleep 10 iste manaki kanipistu excute avuthundi
+
+END_TIME=$(date +%s) 
+
+TOTAL_TIME=$(($END_TIME-$START_TIME)) 
+
+echo "Script executed in: $TOTAL_TIME Seconds"
