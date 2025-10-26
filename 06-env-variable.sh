1f891752339c13e396fd0293dd52979027ffdf48
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-10-26 19:25:56 +0530

    hello world

--- 06-env-variable.sh
+++ 06-env-variable.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+#[ ec2-user@ip-172-31-28-70 ~/shell-sessions- ]$ cd
+#[ ec2-user@ip-172-31-28-70 ~ ]$ ls -la
+#vim .bashrc 
+#enter the variable with value in this file 
+#export COURSE="Shell"
+#if u want save the changes in vim give below cmd
+#source .bashrc
+#logout ayyi login chesi again meeru run chesina same shell ani print avuthundhi
 
 echo "Course name is: $COURSE"
