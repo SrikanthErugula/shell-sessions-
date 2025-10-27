9be516a13374d2ffb72ecd44395b147fb02d3bfc
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-10-27 16:13:19 +0530

    hello world

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,19 +2,19 @@
 
 NUMBER=$1 # edhaina value bayata numchi istunte dhani manam ila cmd tho notify cheyali
 
-if [ $NUMBER -lt 10 ]; then
+#if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is less than 10"
 #elif [ $NUMBER -eq 10 ]; then
     #echo "Given number $NUMBER is equal to 10"
-else
+#else
     echo "Given number $NUMBER is greater than to 10"
-fi
+#fi
 
 # -gt ( greater than symbol)
 # -eq (equal symbol)
 # -ne (not equal)
 
-#practice
+#practice 1
 
 #number=$1
 #if [ $number -lt 10 ] then
@@ -22,3 +22,16 @@ fi
 #else
    # echo "Givem number $number is greater then 10"
    # fi
+#Practice 2
+
+echo "Enter a number:"
+read num
+
+if [ $num -gt 0 ]; then
+    echo "The number is positive."
+elif [ $num -lt 0 ]; then
+    echo "The number is negative."
+else
+    echo "The number is zero."
+fi
+
