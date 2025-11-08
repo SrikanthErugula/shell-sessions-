25c53ee2f94f5302f07920458f17e506adf50d3c
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-08 17:27:08 +0530

    Changes commit

--- 07-cmdvariable.sh
+++ 07-cmdvariable.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#DATE=$(date)
+DATE=$(date)
 
-#echo "Timestamp is: $DATE"
+echo "Timestamp is: $DATE"
 
 
-START_TIME=$(date +%s)
+#START_TIME=$(date +%s)
 
-sleep 2 # So here (sleep 10 &) iste background loki sleep avuthundhi ala kakunda sleep 10 iste manaki foreground kanipistu excute avuthundi
+#sleep 2 # So here (sleep 10 &) iste background loki sleep avuthundhi ala kakunda sleep 10 iste manaki foreground kanipistu excute avuthundi
 
-END_TIME=$(date +%s) 
+#END_TIME=$(date +%s) 
 
-TOTAL_TIME=$(($END_TIME-$START_TIME)) 
+#TOTAL_TIME=$(($END_TIME-$START_TIME)) 
 
-echo "Script executed in: $TOTAL_TIME Seconds"
+#echo "Script executed in: $TOTAL_TIME Seconds"
