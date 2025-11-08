fed8057e502e4ab8b9d1a7bf0ac41b6f912f3262
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-08 17:29:27 +0530

    Changes commit

--- 07-cmdvariable.sh
+++ 07-cmdvariable.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+#ex 1
 DATE=$(date)
 
 echo "Timestamp is: $DATE"
 
-
+#ex 2
 START_TIME=$(date +%s)
 
 sleep 2 # So here (sleep 10 &) iste background loki sleep avuthundhi ala kakunda sleep 10 iste manaki foreground kanipistu excute avuthundi
