b622b513463e0bd6a8c99b651501bf3677a0f5ba
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-08 16:43:05 +0530

    Changes commit

--- 05-pwdvariable.sh
+++ 05-pwdvariable.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # so here sometimes in somewhere it is asking to enter pin nukber like ATM... etc so for that we need to set [pin_number]
 # cmd is read -s (only read iste manam enter chesina pwd visible avuthundhi so ala kakunda -s iste it wont visible to us)
+: << 'END_COMMENT'
 echo "Please enter your PIN number"
 read -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
+END_COMMENT
+
+echo "Enter Pin your number"
+
+read PIN_Number
+echo " Your number is : $PIN_Number"
