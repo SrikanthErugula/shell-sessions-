8970265de6e3c1f1b38a469a1b16d009aecf77a1
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-08 17:20:31 +0530

    Changes commit

--- 06-env-variable.sh
+++ 06-env-variable.sh
@@ -2,7 +2,7 @@
 # so here AWS ec2 instance oka ane server loki ec2-user ane name ichhi login avuthunnam lopala shell script code run 
 # chestunnam like sh ani ichhi so final ga ec2-user anedhi oka parent shell script ki..........aws server oka parent ec2-user ki 
 
-#[ ec2-user@ip-172-31-28-70 ~/shell-sessions- ]$ cd
+#[ ec2-user@ip-172-31-28-70 ~/shell-sessions- ]$ cd ( if u given this it will be directly land into ec2 home folder)
 #[ ec2-user@ip-172-31-28-70 ~ ]$ ls -la
 #vim .bashrc 
 #enter the variable with value in this file like as below
