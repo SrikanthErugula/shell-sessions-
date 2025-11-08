bcf7f40cf027915972b39044e215a2c96ccb4475
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-08 17:40:28 +0530

    Changes commit

--- 08-special-vari.sh
+++ 08-special-vari.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#echo "All variables passed to the script: $@"
-#echo "All variables passed to the script: $*"
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
 echo "Script name: $0" 
-#echo "Current directory: $PWD"
-#echo "Who is running this: $USER"
-#echo "Home directory of user: $HOME"
-#echo "PID of this script: $$" # script ki kuda oka process instance id create avuthundhi we want find then run this cmd
-#sleep 50 &
-#echo "PID of the last command in background is: $!"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$" # script ki kuda oka process instance id create avuthundhi we want find then run this cmd
+sleep 50 &
+echo "PID of the last command in background is: $!"
