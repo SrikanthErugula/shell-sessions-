86c45c2c518986ab541f07eea9af7a2d5390edba
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-09 11:51:27 +0530

    Changes commit

--- 12-installconditions.sh
+++ 12-installconditions.sh
@@ -4,16 +4,16 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0 ( it means failure ane dhani dhaggara zero kakunda emaina ivvachu )
+    #exit 1 # failure is other than 0 ( it means failure ane dhani dhaggara zero kakunda emaina ivvachu )
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
   echo "ERROR:: Installing MySQL is failure"
-    exit 1
+   # exit 1
 else
-   #echo "Installing MySQL is SUCCESS"
+   echo "Installing MySQL is SUCCESS"
     #exit 0 (success ane dhaggara zero ivachu but in shell emi ivvakapoina by defalut it will be consider as zero
 fi
 # sudo sh 12-installconditions.sh  (ila iste mysql install avuthundi)
