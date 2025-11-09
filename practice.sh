724ed443e6d74eb8d095640d55ee7b4870bdf311
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-09 10:42:12 +0530

    Changes commit

--- practice.sh
+++ practice.sh
@@ -2,9 +2,9 @@
 
 Number=$1
 if [ $Number -lt 10 ]; then
-    echo " the given number is less then 10 "
+    echo " the given number $Number is less then 10 "
 elif [ $Number -eq 10 ]; then
-    echo " the given number is equal to 10 "
+    echo " the given number $Number is equal to 10 "
 else 
-    echo " the given number is grater then 10 "
+    echo " the given number $Number is grater then 10 "
 fi
