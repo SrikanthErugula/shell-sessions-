5d5eae00f39876c91038288993c42af948b88067
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-09 10:38:52 +0530

    Changes commit

---
+++ practice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Number=10
+if [ $Number -lt 10 ]; then
+    echo " the given number is less then 10 "
+elif [ $Number -eq 10 ]; then
+    echo " the given number is equal to 10 "
+else 
+    echo " the given number is grater then 10 "
+
