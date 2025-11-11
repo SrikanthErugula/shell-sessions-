283221c6ddd8fc96f616db907f00d24e4745b966
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-11 19:03:39 +0530

    hello shell

---
+++ practice2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sriuser=$(id -u)
+if [ $sriuser -ne 0 ]; then
+    echo " ERROR :: Please run this CMD with root user "
+    exit 1
+fi
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo " Install mysql is FAIL "
+    exit 1
+else
+     echo " Install mysql is SUCCESS"
+fi
