46912570a0b804b1a889e345d0782c58c55ebc4a
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-12 13:24:21 +0530

    hello shell

--- 14-morecolour.sh
+++ 14-morecolour.sh
@@ -23,7 +23,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 dnf list installed mysql
 # Install if it is not found
 if [ $? -ne 0 ]; then
-    dnf install mysql -y
+    dnf remove mysql -y
     VALIDATE $? "MySQL"
 else
     echo -e "MySQL already exist ... $Y SKIPPING $N"
