3fcb1553fc7cd10c561ababd62125c003d8ca4e0
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-12 16:07:51 +0530

    hello shell

--- 15-log.sh
+++ 15-log.sh
@@ -11,7 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/15-log.log
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+echo "Script started executed at: $(date)"
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
