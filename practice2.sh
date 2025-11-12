811095be7475125fa58cded1de9c3332140e440e
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-12 14:52:49 +0530

    hello shell

--- practice2.sh
+++ practice2.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-variuser=$(id -u)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $variuser -ne 0 ]; then 
-    echo " ERROR:: please run with root user "
-    exit 1
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
-SRI(){
-if [ $1 -ne 0 ]; then
-    echo -e " Installing $2 is $R FAIL $N"
-    exit 1
-else
-    echo -e "Installing $2 is $G           SUCCESS $N" # so here entha space iste antha space output lo vastundhi
-fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G  SUCCESS $N"
+    fi
 }
-dnf install mysql -y
-SRI $? "MYSQL"
+
+dnf list installed mysql
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySQL already exist ... $Y SKIPPING $N"
+fi
+
+
