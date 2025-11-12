9b226ffc48e5759a3e5e04f364b43de85576f5fb
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-12 12:55:34 +0530

    hello shell

--- practice2.sh
+++ practice2.sh
@@ -10,17 +10,15 @@ if [ $variuser -ne 0 ]; then
     echo " ERROR:: please run with root user "
     exit 1
 fi
+Sri(){
+    if [ $1 -ne 0 ]; then
+    echo " install $2 is $R FAIl $N "
+    exit 1
+else
+    echo " install $2 is $G Success $N "
+fi
+}
 dnf install mysql -y
-if [ $? -ne 0 ]; then
-    echo " install mysql is $R FAIl $N "
-    exit 1
-else
-    echo " install mysql is $G Success $N "
-fi
+Sri $? "Mysql"
 dnf install nginx -y
-if [ $? -ne 0 ]; then
-    echo " install nginx is $R FAIl $N "
-    exit 1
-else
-    echo " install nginx is $G Success $N "
-fi
+Sri $? "Nginx"
