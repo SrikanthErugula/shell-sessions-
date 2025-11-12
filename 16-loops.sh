d5d5a80e1ff60e252a3dd017d6e62bc22abd1e55
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-12 15:27:15 +0530

    hello shell

--- 16-loops.sh
+++ 16-loops.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-# Iterating from 1 to 5 (inclusive) with a step of 1
-for i in {1..5}; do
-  echo "Iteration $i"
-done
 
-# Iterating from 1 to 5 with a step of 2
-for i in {1..5..2}; do
-  echo "Iteration $i"
-done
-for variable_name in item1 item2 item3; do
-  # Commands to execute for each item
-  echo "Current item: $variable_name"
-done
-for ((i=1; i<=5; i++)); do
-  echo "Count: $i"
+for Sri in {1..50}; do
+echo " the given numbers are $Sri"
 done
