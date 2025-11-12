0ab2dc96611a6a1ad8026f3d91af4a215cc7ff2e
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-12 12:51:52 +0530

    hello shell

--- practice2.sh
+++ practice2.sh
@@ -17,3 +17,10 @@ if [ $? -ne 0 ]; then
 else
     echo " install mysql is $G Success $N "
 fi
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo " install nginx is $R FAIl $N "
+    exit 1
+else
+    echo " install nginx is $G Success $N "
+fi
