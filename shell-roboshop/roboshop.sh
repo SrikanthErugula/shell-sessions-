78b5b7ffa8663ffa3890a0821ae14c2f5c6f918f
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-13 19:05:19 +0530

    hello world

--- shell-roboshop/roboshop.sh
+++ shell-roboshop/roboshop.sh
@@ -6,7 +6,7 @@ DOMAIN_NAME="daws86s.fun"
 
 for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f  --instance-type t3.micro --security-group-ids sg-05184d3f3e9b3610f  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
