9a7b5fe57c29095570d7b257b1e6d4d8fd1d15a0
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-17 18:18:38 +0530

    Hello World

--- shell-roboshop/cata.sh
+++ shell-roboshop/cata.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGO_PV=mangodb.dsoaws.fun
+MONGO_PV=mongodb.dsoaws.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
