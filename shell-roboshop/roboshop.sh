d943ca9f6dd71ed85a947d68ebfc7f7355aa9542
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-18 18:12:33 +0530

    Hellow DEV

--- shell-roboshop/roboshop.sh
+++ shell-roboshop/roboshop.sh
@@ -1,42 +1,17 @@
 #!/bin/bash
-#session 17
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-05184d3f3e9b3610f" # replace with your SG ID
-ZONE_ID="Z0508801ITHFU9ARNA74" # replace with your ID
-DOMAIN_NAME="daws86s.fun"
+#ZONE_ID="Z0508801ITHFU9ARNA74" # replace with your ID
+#DOMAIN_NAME="dsoaws.fun"
 
-for instance in $@ # mongodb redis mysql
+for instance in $@ # mongodb redis mysql ala ivvali names
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-
-    # Get Private IP
-    if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID  --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
-    else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID  --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
-    fi
-
-    echo "$instance: $IP"
-
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record set"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
-        }]
-    }
-    '
+ Instaid=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text) # single code vunte work avvadhu so andhuke "" ivvali at ResourceTyp
+if [ $instance != "frontend" ]; then # frontend kakapothe will get private ip else frontend ite will get pubile ip 
+    IP=$(aws ec2 describe-instances --instance-ids $Instaid  --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+else
+    IP=$(aws ec2 describe-instances --instance-ids $Instaid  --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+fi
+echo " $instance : $IP "
 done
