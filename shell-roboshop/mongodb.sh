b8df740e8ae4af088607ee67d8a1a2680de53c7c
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-18 19:03:46 +0530

    Hellow DEV

--- shell-roboshop/mongodb.sh
+++ shell-roboshop/mongodb.sh
@@ -40,8 +40,8 @@ VALIDATE $? "Enable MongoDB"
 systemctl start mongod 
 VALIDATE $? "Start MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
 VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarted MongoDB"
