5b385151f1d57a1e70019ede104d6f09dc6be8e7
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-19 16:54:09 +0530

    Hellow DEV

--- 19-callscript-1.sh
+++ 19-callscript-1.sh
@@ -5,4 +5,5 @@ NAME=SRIKANTH
 echo " Name is : $NAME"
 echo " PID of Script-1 :: $$"
 
-sh 20-callscript-2.sh
+#sh 20-callscript-2.sh
+source ./20-callscript-2.sh # instead of sh we need to use source and (.) dot represent current directory
