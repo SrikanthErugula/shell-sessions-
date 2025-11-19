c041a21ad643d8ae0cf429cfcb3c316b0a858832
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-19 18:27:37 +0530

    Hellow DEV

--- shell-roboshop/shell-robo-common/common.sh
+++ shell-roboshop/shell-robo-common/common.sh
@@ -11,6 +11,7 @@ LOGS_FOLDER="/var/log/shell-roboshop" # ikkada name chudali okksari
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.dsoaws.fun
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
