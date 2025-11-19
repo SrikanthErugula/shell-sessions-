7f001081ad3a4754231e3069a427593eac52b05b
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-19 15:17:10 +0530

    Hellow DEV

--- shell-roboshop/shipping.sh
+++ shell-roboshop/shipping.sh
@@ -11,7 +11,8 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 START_TIME=$(date +%s)
 END_TIME=$(date +%s)
-MONGODB_HOST=mongodb.dsoaws.fun
+#MONGODB_HOST=mongodb.dsoaws.fun
+MYSQL_HOST=mysql.dsoaws.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
