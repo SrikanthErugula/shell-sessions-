536381880ef0b5bf25fde00bdd22ba75a08403f5
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-19 16:10:40 +0530

    Hellow DEV

---
+++ 19-callscript-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 20-callscript-2 ni 19-callscript-1 lo call chese proccess is like as below
+
+NAME=SRIKANTH
+echo " Name is : $NAME"
+echo " PID of Script-1 :: $$"
+
+sh 20-callscript-2.sh
---
+++ 20-callscript-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+echo " Name is : $NAME"
+echo " PID of Script-1 :: $$"
