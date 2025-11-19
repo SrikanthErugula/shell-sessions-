123d90e63f0d9b766c05ccc6e05dc8e7b02529b5
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-19 13:16:45 +0530

    Hellow DEV

--- shell-roboshop/redis.sh
+++ shell-roboshop/redis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#session 17
+#session 19
 
 USERID=$(id -u)
 R="\e[31m"
@@ -27,3 +27,22 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
         echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7"
+dnf install redis -y  &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf # -e means extra arguments
+VALIDATE $? "Allowing Remote connections to Redis"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
