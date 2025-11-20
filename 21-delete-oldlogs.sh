e50b03751178c3f7727fec0e5fedf699c77b33dc
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-20 12:51:13 +0530

    Hellow while loop

--- 21-delete-oldlogs.sh
+++ 21-delete-oldlogs.sh
@@ -13,7 +13,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-SOURCE_DIR=/home/ec2-user/app_logs
+SOURCE_DIR=/home/ec2-user/app_logs # Source_dir means ekkada numchi files  delete cheyali ani or which folder lo 
 if [ ! -d $SOURCE_DIR ]; then # [ here -d $SOURCE_DIR ila iste dir vunte ichhina CMD excute avuthuni adhe  ( ! -d $SOURCE_DIR ila iste dir ledhu ani error print avuthundi) ]
     echo -e " $R ERROR : Source DIR $SOURCE_DIR doesn't exist $N"
     exit 1
@@ -25,5 +25,8 @@ while IFS= read -r SRI
 do
 
     echo " Deleting the files : $SRI"
+    rm -rf $SRI
+    echo "Deleted the file: $SRI"
+
 
 done <<< $FILES_TO_DELETE # here files ithe just one < symbol chalu but if u want give input then put <<< symbols
