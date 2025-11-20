41c9fddd4c1b73b55e8285c957430303d9025c52
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-20 12:35:57 +0530

    Hellow while loop

--- 22-while-loop.sh
+++ 22-while-loop.sh
@@ -14,15 +14,26 @@
 # # Commands to be executed: This represents the actual commands or statements that form the body of the loop. These commands will run as long as the condition in the while statement is true.
 # done: This keyword marks the end of the while loop's command block.
 
-count=3
+# count=3
 
-echo "Starting countdown..."
+# echo "Starting countdown..."
 
-while [ $count -gt 0 ] # so ikkada 5 num 0 zero kante ekkuva so loop loki velli process start avvuthudi
-do
-  echo "Time left: $count"
-  sleep 1 # Pause for 1 second
-  count=$((count - 1)) # Decrement the count
-done
+# while [ $count -gt 0 ] # so ikkada 5 num 0 zero kante ekkuva so loop loki velli process start avvuthudi
+# do
+#   echo "Time left: $count"
+#   sleep 1 # Pause for 1 second
+#   count=$((count - 1)) # Decrement the count
+# done
 
-echo "Times up!"
+# echo "Times up!"
+
+while IFS= read -r SRI; do # IFS stands for Internal Field Separator. it means by default line by line ni  proper ga  read chestudhi 
+    # Process each line here
+    echo "Processing line: $SRI"
+    # Example: you can perform other operations with the $line variable
+    # For instance:
+    # some_command "$line"
+    # if [[ "$line" == "specific_text" ]]; then
+    #    echo "Found specific text!"
+    # fi
+ done < 19-callscript-1.sh #( here is file_name)
