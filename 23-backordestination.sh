eed6ec517803894fb055bd2a726064fc80f8696d
SrikanthErgula <srikanthreddybtech17@gmail.com>
2025-11-20 13:30:08 +0530

    Hellow while loop

--- 23-backordestination.sh
+++ 23-backordestination.sh
@@ -39,7 +39,7 @@ if [ ! -d $SOURCE_DIR ]; then
      #exit 1
 fi
 
-if [ ! -d $DEST_DIR]; then 
+if [ ! -d $DEST_DIR ]; then 
      echo -e " $R Source DIR $DEST_DIR doesn't exist $N"
      exit 1
 fi
